d899a3683425663887571b483682fd36cb17c515
Chance Zibolski <chance.zibolski@gmail.com>
2014-08-24 19:27:23 -0700

    Add methods to Tree interface. Add generic functions on tree.

--- tree/interface.go
+++ tree/interface.go
@@ -50,4 +50,33 @@ type Node interface {
 
 type Tree interface {
 	Root() Node
+	Search(Orderable) Node
+	Min() Node
+	Max() Node
+	Insert(Orderable)
+	Delete(Orderable)
+}
+
+func Root(tr Tree) Node {
+	return tr.Root()
+}
+
+func Search(tr Tree, v Orderable) Node {
+	return tr.Search(v)
+}
+
+func Min(tr Tree) Node {
+	return tr.Min()
+}
+
+func Max(tr Tree) Node {
+	return tr.Max()
+}
+
+func Insert(tr Tree, v Orderable) {
+	tr.Insert(v)
+}
+
+func Delete(tr Tree, v Orderable) {
+	tr.Delete(v)
 }
